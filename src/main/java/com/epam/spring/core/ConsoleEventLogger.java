330e34323a716c2b53e8461a7edb09b9cc136616
Ihar Sakun <ihar_sakun@epam.com>
2017-07-20 16:10:44 +0300

    add interface EventLogger

--- src/main/java/com/epam/spring/core/ConsoleEventLogger.java
+++ src/main/java/com/epam/spring/core/ConsoleEventLogger.java
@@ -1,7 +1,7 @@
 package com.epam.spring.core;
 
-public class ConsoleEventLogger {
-	void logEvent(String msg) {
+public class ConsoleEventLogger implements EventLogger {
+	public void logEvent(String msg) {
 		System.out.println(msg);
 	}
 }
