c73c23be8ca286db2859c4b9d75c3c64bc694af5
Ihar Sakun <ihar_sakun@epam.com>
2017-07-20 17:01:55 +0300

    add spring.xml

--- src/main/java/com/epam/spring/core/App.java
+++ src/main/java/com/epam/spring/core/App.java
@@ -6,8 +6,8 @@ public class App {
 
 	public static void main(String[] args) {
 		App app = new App();
-		app.client = new Client("1", "Johm Smith");
-		app.eventLogger = new ConsoleEventLogger();
+//		app.client = new Client("1", "Johm Smith");
+//		app.eventLogger = new ConsoleEventLogger();
 		app.logEvent("Some event for user 1");
 	}
 
